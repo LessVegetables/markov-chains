5a455dbe2918d3e2fcd872fb5e356b4f4c9b0bca
LessVegetables <57289239+LessVegetables@users.noreply.github.com>
2026-04-21 15:57:44 +0700

    Add tracking scripts for user interactions
    Added tracking script for button clicks and included a script for analytics.

--- src/app.py
+++ src/app.py
@@ -1,11 +1,16 @@
 import streamlit as st
 from markov_chain.markov_core import MarkovTextGenerator
 
+st.html("""
+    <script defer src="https://stat.gehrman.me/script.js" data-website-id="025ade9a-2a56-494f-a07a-21d6e4b07019"></script>
+""")
+
 default = "И он начал" #ему объяснять
 userInput = st.text_input("Start of Markov Chain", value=default, label_visibility="hidden")
 
 if st.button("Generate"):
-
+    st.html('<script>umami.track("generate-clicked")</script>')
+    
     generator = MarkovTextGenerator(order=len(userInput.split()))
     generator.load_default_dataset()
     output = generator.generate_from_seed(userInput)
